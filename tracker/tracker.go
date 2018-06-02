48d661027c589000de89a7be148522ba183ad2ce
Steven Basgall <sbasgall@gogoair.com>
2018-06-02 10:45:54 -0600

    ???

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -66,7 +66,7 @@ func getTracker(kind string, cfg *config.Monkey) (chaosmonkey.Tracker, error) {
 //		}
 //		fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 	  fmt.Println("slack")
-		return slackobj.tracker, errors.Errorf("unkown error 2")
+		return slackobj{tracker}, errors.Errorf("unkown error 2")
 
 	default:
 		return nil, errors.Errorf("unsupported tracker: %s", kind)
